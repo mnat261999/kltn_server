33450746fbb8c95bf820da23e4c91bf4f7a9823f
lucy <thu.mai@kyanon.digital>
2022-06-16 02:28:51 +0700

    function delete avatar

--- controllers/avatarCtrl.js
+++ controllers/avatarCtrl.js
@@ -6,41 +6,65 @@ const client = algoliasearch('KPC88LV7K0', 'bf9d249b2f37e1507a628474b318f57c')
 
 const avatarCtrl = {
     updateAvatar: async (req, res) => {
-        const {avatar} = req.body
+        try {
+            const { avatar } = req.body
 
-        if(!avatar) return res.status(400).json({success: false, msg: "No avatar upload"})
+            if (!avatar) return res.status(400).json({ success: false, msg: "No avatar upload" })
 
-        const newAvatar = new Avatars({
-            avatar, user:req.user.id
-        })
+            const newAvatar = new Avatars({
+                avatar, user: req.user.id
+            })
 
-        let avatarNew = await newAvatar.save()
+            let avatarNew = await newAvatar.save()
 
-        avatar["avatarId"] = avatarNew._id
+            avatar["avatarId"] = avatarNew._id
 
-        await Users.findOneAndUpdate({_id: req.user.id}, {
-            avatar:avatar
-        })
+            await Users.findOneAndUpdate({ _id: req.user.id }, {
+                avatar: avatar
+            })
 
-        const index = client.initIndex('User')
-
-        index.partialUpdateObject({
-            avatar: avatar,
-            objectID: req.user.id
-        })
-
-        res.status(200).json({ 
-            success:true,
-            msg: "Avatar is uploaded"
-        })
+            res.status(200).json({
+                success: true,
+                msg: "Avatar is uploaded"
+            })
+        } catch (err) {
+            return res.status(500).json({ msg: err.message })
+        }
     },
-    getAllAvatarByUser: async (req, res) =>{
-        const avatars = await Avatars.find({user:req.user.id})
+    getAllAvatarByUser: async (req, res) => {
+        try {
+            const avatars = await Avatars.find({ user: req.user.id })
 
-        res.status(200).json({ 
-            success:true,
-            data:avatars
-        })
+            res.status(200).json({
+                success: true,
+                data: avatars
+            })
+        } catch (err) {
+            return res.status(500).json({ msg: err.message })
+        }
+    },
+    deleteAvatar: async (req, res) => {
+        try {
+            const user = await Users.findById(req.user.id)
+
+            const avatar = await Avatars.findById(req.params.id)
+
+            if (user._id.toString() != avatar.user.toString()) return res.status(400).json({ success: false, msg: "You can only delete avatar created by you" })
+
+            if (user.avatar.avatarId.toString() === req.params.id) {
+                await Users.findOneAndUpdate({ _id: req.user.id }, {
+                    avatar: {}
+                })
+            }
+
+            await Avatars.findByIdAndDelete(req.params.id)
+
+            res.status(200).json({
+                success: true
+            })
+        } catch (err) {
+            return res.status(500).json({ msg: err.message })
+        }
     }
 }
 
--- routes/avatarRouter.js
+++ routes/avatarRouter.js
@@ -7,4 +7,6 @@ router.put('/update', auth, avatarCtrl.updateAvatar)
 
 router.get('/all', auth, avatarCtrl.getAllAvatarByUser)
 
+router.delete('/delete/:id', auth, avatarCtrl.deleteAvatar)
+
 module.exports = router
